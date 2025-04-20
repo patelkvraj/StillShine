fef303f4bf01decfea3ebf27a24257b396d92a10
Vraj Patel <mycsstuff@outlook.com>
2025-04-20 11:13:20 -0400

    Add checkStringEnding() Incroporate into the `Highlight the active page` code.

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
 /* Highlight the active page */
 const currentPage = window.location.pathname;
 const navItemEls = document.querySelectorAll('.link-nav_bar');
-switch (currentPage) {
-    case '/index.html':
-        navItemEls.forEach(el => {
-            el.style.textDecoration = 'none';
-        });
-        document.getElementById('home_header').style.textDecoration = 'underline';
-        document.getElementById('home_footer').style.textDecoration = 'underline';
-        break;
+if (checkStringEnding(currentPage, '/index.html')) {
+    navItemEls.forEach(el => {
+        el.style.textDecoration = 'none';
+    });
+    document.getElementById('home_header').style.textDecoration = 'underline';
+    document.getElementById('home_footer').style.textDecoration = 'underline';
+} else if (checkStringEnding(currentPage, '/about.html')) {
+    navItemEls.forEach(el => {
+        el.style.textDecoration = 'none';
+    });
+    document.getElementById('about_header').style.textDecoration = 'underline';
+    document.getElementById('about_footer').style.textDecoration = 'underline';
+} else if (checkStringEnding(currentPage, '/contact.html')) {
+    navItemEls.forEach(el => {
+        el.style.textDecoration = 'none';
+    });
+    document.getElementById('contact_header').style.textDecoration = 'underline';
+    document.getElementById('contact_footer').style.textDecoration = 'underline';
+} else {
+    navItemEls.forEach(el => {
+        el.style.textDecoration = 'none';
+    });
+}
 
-    case '/about.html':
-        navItemEls.forEach(el => {
-            el.style.textDecoration = 'none';
-        });
-        document.getElementById('about_header').style.textDecoration = 'underline';
-        document.getElementById('about_footer').style.textDecoration = 'underline';
-        break;
-
-    case '/contact.html':
-        navItemEls.forEach(el => {
-            el.style.textDecoration = 'none';
-        });
-        document.getElementById('contact_header').style.textDecoration = 'underline';
-        document.getElementById('contact_footer').style.textDecoration = 'underline';
-        break;
-
-    default:
-        navItemEls.forEach(el => {
-            el.style.textDecoration = 'none';
-        });
-        break;
+/**
+ * Checks if a string ends with a specified substring.
+ * 
+ * 
+ * @param {string} str The string to check.
+ * @param {string} ending ending The substring to check if the `str` ends with.
+ * @returns {boolean} `true` if `str` ends with `ending`, `false` otherwise.
+ */
+function checkStringEnding(str, ending) {
+    return str.endsWith(ending);
 }
