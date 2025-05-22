0682a8040f387d4836a7f81190b664fdbea8fa56
Vraj Patel <mycsstuff@outlook.com>
2025-05-21 22:21:10 -0400

    Add `Product details` to the fetch code. Update the var name for readability. Add new styles respective to it.

--- script.js
+++ script.js
@@ -47,9 +47,31 @@ fetch("https://fakestoreapi.com/products")
     if (productListEl) {
       productListEl.innerHTML = ""; // clear the placeholder
       for (var i = 0; i < 4; i++) {
+        const product = document.createElement("div");
+        product.classList.add("grid-item");
         const productImg = document.createElement("img");
         productImg.src = jsonData[i].image;
-        productListEl.appendChild(productImg);
+        product.appendChild(productImg);
+        /* Product Details */
+        const productDetails = document.createElement("div");
+        productDetails.classList.add("description__product");
+        const productDescription = document.createElement("div"); // Product Description
+        productDescription.classList.add("information__product");
+        const productName = document.createElement("p");
+        productName.classList.add("product__name");
+        productName.innerHTML = jsonData[i].title.substring(0, 10) + "...";
+        productDescription.appendChild(productName);
+        const productLocation = document.createElement("p");
+        productLocation.classList.add("product__location");
+        productLocation.innerHTML = "Lowell, MA";
+        productDescription.appendChild(productLocation);
+        productDetails.appendChild(productDescription);
+        const productAmount = document.createElement("h4"); // Product Amount
+        productAmount.classList.add("amount__product");
+        productAmount.innerHTML = jsonData[i].price;
+        productDetails.appendChild(productAmount);
+        product.appendChild(productDetails);
+        productListEl.appendChild(product);
       }
     }
   })
