c31221a286703db59c05fae753d5e2b06893f737
Vraj Patel <mycsstuff@outlook.com>
2025-05-21 21:25:22 -0400

    Merge api.js and script.js.

--- script.js
+++ script.js
@@ -36,3 +36,23 @@ if (checkStringEnding(currentPage, "/about/")) {
 function checkStringEnding(str, ending) {
   return str.endsWith(ending);
 }
+
+/**
+ * Fetch product data from a fake store API.
+ */
+fetch("https://fakestoreapi.com/products")
+  .then((response) => response.json())
+  .then((jsonData) => {
+    const productListEl = document.getElementById("product_list");
+    if (productListEl) {
+      productListEl.innerHTML = ""; // clear the placeholder
+      for (var i = 0; i < 4; i++) {
+        const productImg = document.createElement("img");
+        productImg.src = jsonData[i].image;
+        productListEl.appendChild(productImg);
+      }
+    }
+  })
+  .catch((error) => {
+    console.log("There is a problem fetching data", error);
+  });
