7bb3ee49be36b39101edc240f60cc73a20879418
Vraj Patel <mycsstuff@outlook.com>
2025-07-08 09:24:48 -0400

    Expect at the root level, replace  index.html to index.php.

---
+++ about/index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>About | Still Shine</title>
+  <link rel="icon" href="../images/Logo-StillShine.svg" type="image/svg" />
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="../index.css" />
+  <link rel="stylesheet" href="./index.css" />
+  <link rel="stylesheet" href="../resp.css" />
+</head>
+
+<body class="body">
+  <div class="page">
+    <header>
+      <div class="content-wrap">
+        <div class="top-navigation">
+          <a class="logo" title="Still Shine" href="../">
+            <img src="../images/Logo-StillShine.svg" alt="Still Shine logo" />
+          </a>
+          <div class="search">
+            <label for="search_bar">
+              Search
+              <img src="../images/icons/search.svg" />
+            </label>
+            <input id="search_bar" type="text" placeholder="Search" />
+            <img src="../images/icons/search.svg" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a
+                  title="Home"
+                  id="home_header"
+                  class="link-nav_bar"
+                  href="../">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  title="About"
+                  id="about_header"
+                  class="link-nav_bar"
+                  href="../about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  title="Contact Us"
+                  id="contact_header"
+                  class="link-nav_bar"
+                  href="../contact">
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a title="Sell" class="button button-sell" href="../sell">
+                  SELL
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="categories">
+          <ul>
+            <li>
+              <span>All Categories</span>
+              <ul>
+                <li>
+                  <span>Mobiles</span>
+                  <ul>
+                    <li>Tablets</li>
+                    <li>Cases & Covers</li>
+                    <li>iPhones</li>
+                    <li>Other - Mobiles</li>
+                  </ul>
+                </li>
+                <li>
+                  <span>Fashion</span>
+                  <ul>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Kids</li>
+                    <li>Other - Fashion</li>
+                  </ul>
+                </li>
+                <li>
+                  <span>Vehicles</span>
+                  <ul>
+                    <li>Tires</li>
+                    <li>Spare Parts</li>
+                    <li>Cars</li>
+                    <li>Motorcycles</li>
+                    <li>Other - Vehicles</li>
+                  </ul>
+                </li>
+                <li>
+                  <span>Wedding</span>
+                  <ul>
+                    <li>Shoes</li>
+                    <li>Clothing</li>
+                    <li>Invitations & Paper</li>
+                    <li>Decorations</li>
+                    <li>Accessories</li>
+                    <li>Other - Wedding</li>
+                  </ul>
+                </li>
+                <li>Art</li>
+                <ul>
+                  <li>Crafts Supplies</li>
+                  <li>Antiques</li>
+                  <li>Collectibles</li>
+                  <li>Handmade</li>
+                  <li>Other - Art</li>
+                </ul>
+              </ul>
+            </li>
+            <li>Electronics</li>
+            <li>Sports & Outdoor</li>
+            <li>Home & Garden</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <!-- Content -->
+    <main class="main-content">
+      <div class="content-wrap">
+        <!-- Hero -->
+        <div
+          id="carouselExampleIndicators"
+          class="carousel slide"
+          data-bs-ride="carousel">
+          <ol class="carousel-indicators">
+            <li
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="0"
+              class="active"></li>
+            <li
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="1"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img
+                class="d-block w-100"
+                src="./images/recycle-trash-can.jpg"
+                alt="First slide" />
+            </div>
+            <div class="carousel-item">
+              <img
+                class="d-block w-100"
+                src="./images/recycle-sea.jpg"
+                alt="Second slide" />
+            </div>
+          </div>
+          <a
+            class="carousel-control-prev"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-bs-slide="prev">
+            <span
+              class="carousel-control-prev-icon"
+              aria-hidden="true"></span>
+          </a>
+          <a
+            class="carousel-control-next"
+            href="#carouselExampleIndicators"
+            role="button"
+            data-bs-slide="next">
+            <span
+              class="carousel-control-next-icon"
+              aria-hidden="true"></span>
+          </a>
+        </div>
+        <!-- Main content -->
+        <div class="grid-item main">
+          <p class="information">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has
+            survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was
+            popularised in the 1960s with the release of Letraset sheets
+            containing Lorem Ipsum passages, and more recently with desktop
+            publishing software like Aldus PageMaker including versions of
+            Lorem Ipsum
+          </p>
+          <div class="information__border"></div>
+          <button class="button about__button">All Categories</button>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div class="content-wrap">
+        <div class="footer__top">
+          <div class="newsletter-and-navigation">
+            <!--Newsletter-->
+            <div class="newsletter">
+              <p>
+                Sign up to check out the latest products that go for sale and
+                many more...
+              </p>
+              <label for="email__newsletter">Email address</label>
+              <input
+                id="email__newsletter"
+                placeholder="  Email address"
+                type="email" />
+              <button class="button">SIGN UP</button>
+            </div>
+            <!--Navbar-->
+            <div class="navbar__footer">
+              <h4>Sitemap</h4>
+              <p>All our pages</p>
+              <nav class="navbar__footer__links">
+                <ul class="flex_container_navbar_footer">
+                  <li>
+                    <a id="home_footer" class="link-nav_bar" href="../">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      id="about_footer"
+                      class="link-nav_bar"
+                      href="../about/">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      id="contact_footer"
+                      class="link-nav_bar"
+                      href="../contact/">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <!--Branding-->
+          <div class="branding">
+            <a class="logo" href="../">
+              <img
+                src="../images/Logo-StillShine.svg"
+                alt="Still Shine logo" />
+            </a>
+            <p>Earn money and save the world at the same time.</p>
+          </div>
+        </div>
+        <div class="legal__socialLinks">
+          <!--Legal-->
+          <div class="footer-legal horizontal-list">
+            <!--Copyright-->
+            <p>&copy; StillShine 2024</p>
+            <!--Policies-->
+            <p>Privacy policy</p>
+            <p>Terms of use</p>
+          </div>
+          <!--Social Links-->
+          <div class="social-links horizontal-list">
+            <a href="https://www.facebook.com/" target="_blank">
+              <img src="../images/icons/facebook.svg" />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank">
+              <img src="../images/icons/instagram.svg" />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank">
+              <img src="../images/icons/youtube.svg" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="../script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
---
+++ contact/index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Contact | Still Shine</title>
+  <link rel="icon" href="../images/Logo-StillShine.svg" type="image/svg" />
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+    crossorigin="anonymous" />
+  <link rel="stylesheet" href="../index.css" />
+  <link rel="stylesheet" href="./index.css" />
+  <link rel="stylesheet" href="../resp.css" />
+</head>
+
+<body class="body">
+  <div class="page">
+    <header>
+      <div class="content-wrap">
+        <div class="top-navigation">
+          <a class="logo" title="Still Shine" href="../">
+            <img src="../images/Logo-StillShine.svg" alt="Still Shine logo" />
+          </a>
+          <div class="search">
+            <label for="search_bar">
+              Search
+              <img src="../images/icons/search.svg" />
+            </label>
+            <input id="search_bar" type="text" placeholder="Search" />
+            <img src="../images/icons/search.svg" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a
+                  title="Home"
+                  id="home_header"
+                  class="link-nav_bar"
+                  href="../">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  title="About"
+                  id="about_header"
+                  class="link-nav_bar"
+                  href="../about">
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  title="Contact Us"
+                  id="contact_header"
+                  class="link-nav_bar"
+                  href="../contact">
+                  Contact Us
+                </a>
+              </li>
+              <li>
+                <a title="Sell" class="button button-sell" href="../sell">
+                  SELL
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="categories">
+          <ul>
+            <li>
+              <span>All Categories</span>
+              <ul>
+                <li>
+                  <span>Mobiles</span>
+                  <ul>
+                    <li>Tablets</li>
+                    <li>Cases & Covers</li>
+                    <li>iPhones</li>
+                    <li>Other - Mobiles</li>
+                  </ul>
+                </li>
+                <li>
+                  <span>Fashion</span>
+                  <ul>
+                    <li>Men</li>
+                    <li>Women</li>
+                    <li>Kids</li>
+                    <li>Other - Fashion</li>
+                  </ul>
+                </li>
+                <li>
+                  <span>Vehicles</span>
+                  <ul>
+                    <li>Tires</li>
+                    <li>Spare Parts</li>
+                    <li>Cars</li>
+                    <li>Motorcycles</li>
+                    <li>Other - Vehicles</li>
+                  </ul>
+                </li>
+                <li>
+                  <span>Wedding</span>
+                  <ul>
+                    <li>Shoes</li>
+                    <li>Clothing</li>
+                    <li>Invitations & Paper</li>
+                    <li>Decorations</li>
+                    <li>Accessories</li>
+                    <li>Other - Wedding</li>
+                  </ul>
+                </li>
+                <li>Art</li>
+                <ul>
+                  <li>Crafts Supplies</li>
+                  <li>Antiques</li>
+                  <li>Collectibles</li>
+                  <li>Handmade</li>
+                  <li>Other - Art</li>
+                </ul>
+              </ul>
+            </li>
+            <li>Electronics</li>
+            <li>Sports & Outdoor</li>
+            <li>Home & Garden</li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <!-- Main content -->
+    <main class="main-content">
+      <div class="content-wrap">
+        <div class="grid-item main">
+          <h1>Connect With Us</h1>
+          <p class="message">
+            Simply leave a message and we'll get right back to you.
+          </p>
+          <form>
+            <label for="full_name">Full name*</label>
+            <input id="full_name" placeholder="Full name*" /><br />
+            <label for="phone_number">Phone number</label>
+            <input id="phone_number" placeholder="Phone number" /><br />
+            <label for="email_address">Email address*</label>
+            <input id="email_address" placeholder="Email address*" /><br />
+            <label for="message">Your message*</label>
+            <textarea
+              id="message"
+              placeholder="Your message*"
+              rows="4"
+              cols="20"></textarea>
+            <br />
+            <button class="button contact__button">Submit</button>
+            <p>
+              <img src="../images/icons/lock.svg" />We never share your
+              private date.
+            </p>
+          </form>
+        </div>
+      </div>
+    </main>
+    <footer>
+      <div class="content-wrap">
+        <div class="footer__top">
+          <div class="newsletter-and-navigation">
+            <!--Newsletter-->
+            <div class="newsletter">
+              <p>
+                Sign up to check out the latest products that go for sale and
+                many more...
+              </p>
+              <label for="email__newsletter">Email address</label>
+              <input
+                id="email__newsletter"
+                placeholder="  Email address"
+                type="email" />
+              <button class="button">SIGN UP</button>
+            </div>
+            <!--Navbar-->
+            <div class="navbar__footer">
+              <h4>Sitemap</h4>
+              <p>All our pages</p>
+              <nav class="navbar__footer__links">
+                <ul class="flex_container_navbar_footer">
+                  <li>
+                    <a id="home_footer" class="link-nav_bar" href="../">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      id="about_footer"
+                      class="link-nav_bar"
+                      href="../about/">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      id="contact_footer"
+                      class="link-nav_bar"
+                      href="../contact/">
+                      Contact Us
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+          <!--Branding-->
+          <div class="branding">
+            <a class="logo" href="../">
+              <img
+                src="../images/Logo-StillShine.svg"
+                alt="Still Shine logo" />
+            </a>
+            <p>Earn money and save the world at the same time.</p>
+          </div>
+        </div>
+        <div class="legal__socialLinks">
+          <!--Legal-->
+          <div class="footer-legal horizontal-list">
+            <!--Copyright-->
+            <p>&copy; StillShine 2024</p>
+            <!--Policies-->
+            <p>Privacy policy</p>
+            <p>Terms of use</p>
+          </div>
+          <!--Social Links-->
+          <div class="social-links horizontal-list">
+            <a href="https://www.facebook.com/" target="_blank">
+              <img src="../images/icons/facebook.svg" />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank">
+              <img src="../images/icons/instagram.svg" />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank">
+              <img src="../images/icons/youtube.svg" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="../script.js"></script>
+</body>
+
+</html>
